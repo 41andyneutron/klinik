6262dc705e01186953953fe019a8739862edfc47
Andy Neutron <32219104+41andyneutron@users.noreply.github.com>
2025-04-18 21:09:28 +0700

    Add files via upload

---
+++ protected/views/obat/_view.php
@@ -0,0 +1,29 @@
+<?php
+/* @var $this ObatController */
+/* @var $data Obat */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id_obat')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id_obat), array('view', 'id'=>$data->id_obat)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nama_obat')); ?>:</b>
+	<?php echo CHtml::encode($data->nama_obat); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('deskripsi')); ?>:</b>
+	<?php echo CHtml::encode($data->deskripsi); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('stok')); ?>:</b>
+	<?php echo CHtml::encode($data->stok); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('harga')); ?>:</b>
+	<?php echo CHtml::encode($data->harga); ?>
+	<br />
+
+
+</div>
