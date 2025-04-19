686cbba70c01b30746168e6918e80a38ad8dc25c
Andy Neutron <32219104+41andyneutron@users.noreply.github.com>
2025-04-19 08:28:22 +0700

    Add files via upload

---
+++ protected/views/pasien/_search.php
@@ -0,0 +1,50 @@
+<?php
+/* @var $this PasienController */
+/* @var $model Pasien */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'nik'); ?>
+		<?php echo $form->textField($model,'nik',array('size'=>16,'maxlength'=>16)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'nama_pasien'); ?>
+		<?php echo $form->textField($model,'nama_pasien',array('size'=>60,'maxlength'=>100)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'tempat_lahir'); ?>
+		<?php echo $form->textField($model,'tempat_lahir',array('size'=>60,'maxlength'=>100)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'tanggal_lahir'); ?>
+		<?php echo $form->textField($model,'tanggal_lahir'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'jk'); ?>
+		<?php echo $form->textField($model,'jk',array('size'=>1,'maxlength'=>1)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'tanggal_daftar'); ?>
+		<?php echo $form->textField($model,'tanggal_daftar'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
