1b32afec1e63e640c7c857e4cffd005f52c696b4
Andy Neutron <32219104+41andyneutron@users.noreply.github.com>
2025-04-19 08:28:08 +0700

    Add files via upload

---
+++ protected/views/pasien/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this PasienController */
+/* @var $data Pasien */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nik')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->nik), array('view', 'id'=>$data->nik)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nama_pasien')); ?>:</b>
+	<?php echo CHtml::encode($data->nama_pasien); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('tempat_lahir')); ?>:</b>
+	<?php echo CHtml::encode($data->tempat_lahir); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('tanggal_lahir')); ?>:</b>
+	<?php echo CHtml::encode($data->tanggal_lahir); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('jk')); ?>:</b>
+	<?php echo CHtml::encode($data->jk); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('tanggal_daftar')); ?>:</b>
+	<?php echo CHtml::encode($data->tanggal_daftar); ?>
+	<br />
+
+
+</div>
