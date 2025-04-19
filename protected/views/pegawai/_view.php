5d3a9bbd18c5a1e5710393b3e37ebbc42f4f596a
Andy Neutron <32219104+41andyneutron@users.noreply.github.com>
2025-04-19 13:37:40 +0700

    Add files via upload

---
+++ protected/views/pegawai/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this PegawaiController */
+/* @var $data Pegawai */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id_pegawai')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id_pegawai), array('view', 'id'=>$data->id_pegawai)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nama_pegawai')); ?>:</b>
+	<?php echo CHtml::encode($data->nama_pegawai); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('tempat_lahir')); ?>:</b>
+	<?php echo CHtml::encode($data->tempat_lahir); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('tanggal_lahir')); ?>:</b>
+	<?php echo CHtml::encode($data->tanggal_lahir); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('jk')); ?>:</b>
+	<?php echo CHtml::encode($data->jk); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('tanggal_masuk')); ?>:</b>
+	<?php echo CHtml::encode($data->tanggal_masuk); ?>
+	<br />
+
+
+</div>
